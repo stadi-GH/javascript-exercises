c73391234a0ef81e0b8ccc611a6b98d8c1c447b3
stadi-GH <aweskadoubleface0@gmail.com>
2026-09-22 08:33:00 +0300

    Update removeFromArray.js
    used the rest parameter syntax and the items to remove, to remove an item from an array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
 const removeFromArray = function() {
 };
-
+function removeArgs(arr, ...itemsToRemove) {
+    return arr.filter(function(item) {
+        return !itemsToRemove.includes(item);
+    });
+}
+console.log(removeArgs([1, 2, 3, 4], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
